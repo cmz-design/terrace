391f9bb44f62ac57312b7a31b2a2df64aa92bf61
910958809@qq.com <910958809@qq.com>
2023-08-28 17:53:49 +0800

    1

--- test.sh
+++ test.sh
@@ -4,20 +4,20 @@
 #     CILK_NWORKERS=28 ./graph_bm -f $file -r 1000000  -a 1 >> static2.txt
 # done
 
-for bs in {1000000,10000000,100000000}
-do
-    for file in `ls ~/data/large/*adj`
-    do
-        CILK_NWORKERS=28 ./graph_bm_static -f $file -r $bs -a 0 >> static2.txt
-        CILK_NWORKERS=28 ./graph_bm -f $file -r $bs  -a 0 >> static2.txt
-    done
+# for bs in {1000000,10000000,100000000}
+# do
+#     for file in `ls ~/data/large/*adj`
+#     do
+#         CILK_NWORKERS=28 ./graph_bm_static -f $file -r $bs -a 0 >> static2.txt
+#         CILK_NWORKERS=28 ./graph_bm -f $file -r $bs  -a 0 >> static2.txt
+#     done
 
-    for file in `ls ~/data/large/*txt`
-    do
-        CILK_NWORKERS=28 ./graph_bm_static -f $file -r $bs -a 1 >> static2.txt
-        CILK_NWORKERS=28 ./graph_bm -f $file -r $bs  -a 1 >> static2.txt
-    done
-done
+#     for file in `ls ~/data/large/*txt`
+#     do
+#         CILK_NWORKERS=28 ./graph_bm_static -f $file -r $bs -a 1 >> static2.txt
+#         CILK_NWORKERS=28 ./graph_bm -f $file -r $bs  -a 1 >> static2.txt
+#     done
+# done
 
 # for file in `ls ~/data/undirected/*adj`
 # do
@@ -25,4 +25,4 @@ done
 #     CILK_NWORKERS=28 ./graph_bm -f $file -r 100000  -a 2 >> static2.txt
 # done
 
-#  CILK_NWORKERS=28 ./graph_bm -f ~/data/wiki-topcats.txt -r 100000 -a 1 >> static2.txt
+ CILK_NWORKERS=28 ./graph_bm -f ~/data/large/LJ_sym.adj -r 100000000 -a 0 >> static2.txt
